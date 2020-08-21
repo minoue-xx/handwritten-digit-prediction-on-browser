05088f63798e3f7baa59c46724026954a237548b
minoue <minoue@TYO-MINOUE.dhcp.mathworks.com>
2020-08-22 07:17:39 +0900

    created Projects for generating WebAsssembly

---
+++ trainingModels/generateWebAssembly/render.m
@@ -0,0 +1,14 @@
+function render()
+%RENDER Runs in an infinite loop
+%   The render function is run in an infinite loop at a specified update
+%   rate. Use the render function to make updates to graphics and dom
+%   objects. You can also use the render() function to perform simple
+%   polling of data. The render function takes zero inputs and zero
+%   outputs.
+
+%
+% Copyright 2019 The MathWorks, Inc.
+%
+
+
+end
---
+++ trainingModels/generateWebAssembly/start.m
@@ -0,0 +1,14 @@
+function start()
+%START Run once at the start of the app
+%   The start function is run once at the start of the app to initialize
+%   values or perform other setup operations. The start function takes no
+%   inputs and no outputs.
+
+%
+% Copyright 2019 The MathWorks, Inc.
+%
+
+
+
+end
+
